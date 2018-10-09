ea9d6e6711e39e210cb8d13a288668382871a500
teste <teste@2ttes.com>
2018-10-09 14:01:18 -0300

    Regras do jogo funcionando. Criação de Modal (Em Progresso)

--- game.js
+++ game.js
@@ -1,4 +1,5 @@
 game = () => {
+    let solved = false;
     let mat = [];    
     const pieces = [
         {'content' : '1'}, 
@@ -25,16 +26,48 @@ game = () => {
         mat[2][0] = pieces[6];
         mat[2][1] = pieces[7];
         mat[2][2] = pieces[8];
+
+        // mat = shuffle(mat);
+        // while (isSolved()) {
+        //     mat = shuffle(mat);
+        // }
     }
+
+    /**
+     * Randomly shuffle an array
+     * https://stackoverflow.com/a/2450976/1293256
+     * @param  {Array} array The array to shuffle
+     * @return {String}      The first item in the shuffled array
+     */
+    shuffle = function (array) {
+
+        var currentIndex = array.length;
+        var temporaryValue, randomIndex;
+
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+
+    };
 
     render = () => {
         const elemInteractArea = document.getElementsByClassName('interact-area')[0];
         const elemUl = document.createElement('ul');
 
         elemInteractArea.innerHTML = '';
+        elemUl.innerHTML = '';
 
         elemUl.className = "container";
-        elemInteractArea.className += " container";
     
         for (let i = 0; i < mat.length ;i++) {
             for (let j = 0; j < mat.length; j++) { // Cria element
@@ -53,6 +86,28 @@ game = () => {
     
         elemInteractArea.appendChild(elemUl);
     
+    }
+
+    isSolved = () => {
+        let prevNum = 0;
+        let solved = true;
+
+        for (let i = 0; i < mat.length; i++) {
+            for (let k = 0; k < mat.length; k++) {
+                if ((mat[i][k].content-1) != prevNum && mat[i][k].content != '') {
+                    solved = false;
+                    break;
+                }
+                prevNum++;
+            }
+
+            if (!solved) break;
+        }
+
+        return new Promise((resolve, reject) => 
+        {
+            resolve(solved);
+        });
     }
 
     validateMovement = (row, col) => {
@@ -90,6 +145,11 @@ game = () => {
         }
 
         render();
+        pSolved = isSolved();
+        pSolved.then((solved) => {
+            if (solved) alert('Resolvido');
+        });
+        
     }
 
     getMat = () => {
